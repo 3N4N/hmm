6afcdf3f2d3a8f84244f6ee7cc39de5c35dadfa2
Enan Ajmain <3nan.ajmain@gmail.com>
2022-01-04 02:23:54 +0600

    Implement Viterbi algorithm

--- viterbi.py
+++ viterbi.py
@@ -18,47 +18,70 @@ def load_param(fn_param):
 
     n_states = int(fparam.readline().strip())
 
-    P = None
+    Tm = None
     for i in range(n_states):
         p_ij = list(map(float, fparam.readline().strip().split('\t')))
-        if P is None:
-            P = np.array([p_ij])
+        if Tm is None:
+            Tm = np.array([p_ij])
         else:
-            P = np.concatenate((P, [p_ij]))
+            Tm = np.concatenate((Tm, [p_ij]))
 
-    mu = list(map(float, fparam.readline().strip().split('\t')))
-    sd = list(map(float, fparam.readline().strip().split('\t')))
+    mu = np.array(list(map(float, fparam.readline().strip().split('\t'))))
+    sd = np.sqrt(np.array(list(map(float, fparam.readline().strip().split('\t')))))
 
     fparam.close()
-    return n_states, P, mu, sd
+    return n_states, Tm, mu, sd
 
 
-def get_stationary_distribution(P, n):
-    y = P
+def get_stationary_distribution(Tm, n):
+    y = Tm
     for _ in range(n):
-        y = np.matmul(y, P)
+        y = np.matmul(y, Tm)
     return y
 
 
-def viterbi(y, P, E, Pi):
-    T = len(y)
+def viterbi(y, Tm, Em, Pi):
+    N = len(y)
+    S = Tm.shape[0]
+
+    trellis = np.zeros((N, S))
+    trellis[0, :] = np.log(Pi, Em[:, 0])
+    pointers = np.zeros((N-1, S)).astype(np.int32)
+
+    for n in range(1, N):
+        for s in range(S):
+            probability = trellis[n - 1] + np.log(Tm[:, s]) + np.log(Em[s, n])
+            pointers[n-1, s] = np.argmax(probability)
+            trellis[n, s] = np.max(probability)
+
+    S_opt = np.zeros(N).astype(np.int32)
+    S_opt[-1] = np.argmax(trellis[N-1, :])
+
+    for n in range(N-2, -1, -1):
+        S_opt[n] = pointers[n, int(S_opt[n+1])]
+
+    return S_opt
 
 
 
 
 y = load_data("data/input/data.txt")
-N, P, mu, sd = load_param("data/input/parameters.txt")
-print(N, P, mu, sd, sep="\n", end="\n\n")
+S, Tm, mu, sd = load_param("data/input/parameters.txt")
 
-Pi = get_stationary_distribution(P, 50)
-print(Pi)
+Pi = get_stationary_distribution(Tm, 50)
 Pi = Pi[0]
-print(Pi)
 
-E = None
-for i in range(N):
-    if E is None:
-        E = [np.array(norm(mu[i],sd[i]).pdf(y))]
+Em = None
+for s in range(S):
+    probs = np.array(norm(mu[s],sd[s]).pdf(y))
+    if Em is None:
+        Em = [probs]
     else:
-        E = np.concatenate((E, [np.array(norm(mu[i],sd[i]).pdf(y))]))
-print(E.shape)
+        Em = np.concatenate((Em, [probs]))
+# np.savetxt("Em.txt", Em)
+
+
+S_opt = viterbi(y, Tm, Em, Pi).astype(str)
+S_opt[S_opt == '0'] = "El Nino"
+S_opt[S_opt == '1'] = "La Nina"
+np.savetxt("data/output/states.txt", S_opt, fmt="\"%s\"")
